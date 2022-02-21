d32760392113d6c34cedab13c19fc8858b7d98d5
Mateo <madiazt@unal.edu.co>
2022-02-20 21:22:11 -0500

    added post comment functionality

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,20 +1,22 @@
 import { useState } from "react"
-import { Typography } from "@mui/material"
+import { Typography, TextField, Button, Paper, Divider } from "@mui/material"
 import CommentPaper from "./CommentPaper";
 
 const CommentSection = (props) => {
     const publishComment = props.publishComment;
     const comments = props.comments
 
-    const [text, setText] = useState();
+    const [text, setText] = useState("");
 
     const handleText = (event) => {
         setText(event.target.value)
     }
-    const handleForm = () => {
+    const handleForm = (event) => {
+        event.preventDefault();
         const newComment = {
             text
         }
+        setText("");
         publishComment(newComment);
     }
     if (comments === undefined) {
@@ -25,6 +27,25 @@ const CommentSection = (props) => {
         return (
             <div  style={{ padding: 14 }}>
                 <Typography variant="h5">Comment Section</Typography>
+                <Paper style={{ padding: "20px 15px" }} >
+                    <form onSubmit={handleForm}>
+                        <TextField
+                            fullWidth
+                            label="Comment"
+                            value={text}
+                            onChange={handleText}
+                        >
+                        </TextField>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            type="submit"
+                        >
+                            Publish comment
+                        </Button>
+                    </form>
+                </Paper>
+                <Divider variant="fullWidth" style={{ margin: "20px 0" }} />
                 {comments.length !== 0 ? comments.map((comment) => {return <CommentPaper key={comment._id} comment={comment} />}) :
                     <div>There are no comments yet. Publish one!</div>}
             </div>
